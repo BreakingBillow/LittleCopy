7598f9d4a0144d3ca5302e83e2f5c91bbc478f4a
breakingbillow <house.woo@gmail.com>
2016-03-23 17:43:27 +0800

    First commit

---
+++ LittleCopy/src/main/java/com/explitech/littlecopy/rest/UserApi.java
@@ -0,0 +1,35 @@
+package com.explitech.littlecopy.rest;
+
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+import org.codehaus.jackson.map.annotate.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Path("/user")
+public class UserApi {
+	
+    @Context
+    UriInfo uriInfo;
+
+    @Context
+    Request request;
+
+    
+    
+}
---
+++ LittleCopy/src/main/resources/databasae.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+`user_id` int(11) NOT NULL auto_increment,
+`use_name` varchar(20) NOT NULL,
+`password` varchar(50) NOT NULL,
+PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `order` (
+`order_id` int(11) NOT NULL auto_increment,
+`order_start_date` datetime NOT NULL,
+`order_end_date` datetime NOT NULL,
+`contract_type_id` int(4) NOT NULL,
+PRIMARY KEY (`order_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Config` (
+`config_id` int(11) NOT NULL auto_increment,
+`config_name` varchar(50) NOT NULL,
+`config_value` varchar(50) NOT NULL,
+PRIMARY KEY (`config_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `order_history` (
+`history_id` int(11) NOT NULL auto_increment,
+`date` datetime NOT NULL,
+`order_number` varchar(50) NOT NULL,
+PRIMARY KEY (`history_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `storage` (
+`record_id` int(11) NOT NULL auto_increment,
+`date` datetime NOT NULL,
+`current` varchar(50) NOT NULL,
+PRIMARY KEY (`record_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `utilization` (
+`record_id` int(11) NOT NULL auto_increment,
+`step` int(11) NOT NULL,
+`date` datetime NOT NULL,
+`value` varchar(50) NOT NULL,
+PRIMARY KEY (`record_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `leadtime` (
+`leadtime_id` int(11) NOT NULL auto_increment,
+`date` datetime NOT NULL,
+`value` varchar(50) NOT NULL,
+PRIMARY KEY (`leadtime_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `revenue` (
+`revenue_id` int(11) NOT NULL auto_increment,
+`contract_type_id` int(4) NOT NULL,
+`date` datetime NOT NULL,
+`value` varchar(50) NOT NULL,
+PRIMARY KEY (`revenue_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
